799d7dd42da9c5383e4dfc6226b424bc4c3395dd
tomatolian <vinegtom@gmail.com>
2021-11-09 20:42:01 +0900

    debug and add close func

--- ato/js/script.js
+++ ato/js/script.js
@@ -4,11 +4,22 @@ $(function(){
         var img_pass = $(this).children("img").attr('src')
         console.log(img_pass)
         $(".modal-img").attr("src",img_pass)
+        wait(3).done(function(){
+        $('.js-modal').fadeOut();    
+        });
         return false;
     });
     $('.js-modal-close').on('click',function(){
         $('.js-modal').fadeOut();
         return false;
     });
+    function wait(sec) {
 
+        var objDef = new $.Deferred;
+ 
+        setTimeout(function () {
+            objDef.resolve(sec);
+        }, sec*1000);
+        return objDef.promise();
+    };
 });
