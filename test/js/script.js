9551a9b5ae79ad96fee1bc6843c4426082430b3f
tomatolian <vinegtom@gmail.com>
2022-05-17 20:17:24 +0900

    a

--- test/js/script.js
+++ test/js/script.js
@@ -2,5 +2,9 @@ scene = new THREE.Scene();
 obj=new THREE.GLTFLoader()
    .setPath( '/test/models/' )
    .load( 'Flower.glb', function ( gltf ) {
-      scene.add( gltf.scene);
-   }).scale.set(100, 100, 100); 
+    objmodel = gltf.clone();
+    objmodel.scale.set(100, 100, 100);
+    objmodel.rotation.set(0, 0, 0); 
+    objmodel.position.set(0, 0, 0);   
+    scene.add(objmodel);
+   }); 
