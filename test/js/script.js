b5018e5a831d6e0bd4b56ea079121e8ca70ebd94
tomatolian <vinegtom@gmail.com>
2022-05-17 15:28:38 +0900

    test

--- test/js/script.js
+++ test/js/script.js
@@ -1,5 +1,9 @@
 const loader = new THREE.TDSLoader();
-loader.setResourcePath('models/textures');
-loader.load('models/Cow_N050213.3DS',  (object) => {
-scene.add(object);
+const url = 'models/Cow_N050213.3DS';
+loader.load(url, function(object){
+  objmodel = object.clone();
+  objmodel.scale.set(1,1,1);//縮尺の初期化
+  objmodel.rotation.set(0,0,135);//角度の初期化
+  objmodel.position.set(-100,0,0);//位置の初期化
+  scene.add(objmodel);
 });
