29adf4cb3244c77022d2ac924605c86b21b38edd
tomatolian <vinegtom@gmail.com>
2022-05-17 21:56:02 +0900

    a

--- test/js/script.js
+++ test/js/script.js
@@ -28,7 +28,6 @@ function init() {
    .setPath( '/test/models/' )
    .load( 'Flower.glb', function ( gltf ) {
     gltf.scene.scale.set(100,100,100)
-    scene.add(gltf.scene);
    }); 
   const material = new THREE.MeshStandardMaterial({
     color: 0x0000ff
