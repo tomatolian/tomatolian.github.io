92cb12785612404a58c9cda2cb58a8555e8b775c
tomatolian <vinegtom@gmail.com>
2022-05-17 18:58:36 +0900

    test

--- test/js/script.js
+++ test/js/script.js
@@ -1,9 +1,5 @@
-const loader = new THREE.TDSLoader();
-const url = 'models/Cow_N050213.3DS';
-loader.load(url, function(object){
-  objmodel = object.clone();
-  objmodel.scale.set(1,1,1);//縮尺の初期化
-  objmodel.rotation.set(0,0,135);//角度の初期化
-  objmodel.position.set(-100,0,0);//位置の初期化
-  scene.add(objmodel);
-});
+new THREE.GLTFLoader()
+   .setPath( '../models/' )
+   .load( 'Flower.glb', function ( gltf ) {
+      scene.add( gltf.scene );
+   });
