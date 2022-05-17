b9809e0b386852a1de8c897dce985849f672cd15
tomatolian <vinegtom@gmail.com>
2022-05-17 20:50:44 +0900

    test

--- test/js/script.js
+++ test/js/script.js
@@ -1,9 +1,54 @@
-scene = new THREE.Scene(); 
-var axes = new THREE.AxesHelper(25);
-scene.add(axes);
-obj=new THREE.GLTFLoader()
-   .setPath( '/test/models/' )
-   .load( 'Flower.glb', function ( gltf ) {
-    gltf.scene.scale.set(100,100,100)
-    scene.add(gltf.scene);
-   }); 
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas")
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    width / height,
+    1,
+    10000
+  );
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(500, 500, 500);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x0000ff
+  });
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  // 平行光源
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.intensity = 2; // 光の強さを倍に
+  light.position.set(1, 1, 1);
+  // シーンに追加
+  scene.add(light);
+
+  // 初回実行
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    // 箱を回転させる
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    // レンダリング
+    renderer.render(scene, camera);
+  }
+}
